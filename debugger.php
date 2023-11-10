707ed2553a7bfbc103030849997c5d8a51ece778
Eugene van der Merwe <eugenevdm@gmail.com>
2023-11-10 15:53:09 +0200

    remove comments in debugger

--- debugger.php
+++ debugger.php
@@ -1,24 +1,21 @@
 <?php
 /**
- * Register a debugger. This debugger shows output and can take three
- * parameters, the message, a variable, and mode (.e.g DEBUG)
+ * A basic debugger that writes output to a file.
+ * It takes three parameters:
+ * The message to output, a variable to output, and a mode (.e.g DEBUG, INFO, ERROR, etc)
  */
 if (!function_exists('debugger')) {
     function debugger($message, $variable = '', $mode = "DEBUG")
     {
-        global $plugin_name;
-
         if (get_option('forms_api_enable_debugger') !== "1") {
             return;
         }
 
-
-
-        $serverAddr     = $_SERVER['SERVER_ADDR'];
+        $serverAddr = $_SERVER['SERVER_ADDR'];
 
         $dateTimeFormat = date('Y-m-d H:i:s');
 
-        $prefix         = "[$dateTimeFormat] $serverAddr.$mode: ";
+        $prefix = "[$dateTimeFormat] $serverAddr.$mode: ";
 
         if (is_array($variable) or is_object($variable)) {
             $variable = print_r($variable, 1);
@@ -27,16 +24,7 @@ if (!function_exists('debugger')) {
         }
 
         $logPath = (dirname(__FILE__)) . '/';
-//        die($logPath);
-//        $logPath = ABSPATH . "wp-content/plugins/$plugin_name/";
-                                
-        // file_put_contents($logPath . 'log_' . date("dmY") . '.log', $prefix . $message . $variable . "\n", FILE_APPEND);
+
         file_put_contents($logPath . 'log.txt', $prefix . $message . $variable . "\n", FILE_APPEND);
     }
 }
-
-/**
- * TODO
- * 
- * - Add option to output date according to locale
- */
